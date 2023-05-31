5fcb6cdffdf17be849241881ccf15474d2fc36b5
Lee Watson <watsonalee@gmail.com>
2023-05-31 17:28:37 +0100

    build

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,3 @@
-// import { useState } from "react";
-// import Link from "next/link";
-// import Layout from "@/components/Layout";
-// import Image from "next/image";
-// import Head from "next/head";
-// import styles from "../styles/Home.module.css";
-
-// export default function Home() {
-//   return (
-//     <>
-//       <Head>
-//         <title>Pokedex | Home</title>
-//         <meta name="keywords" content="pokemon" />
-//       </Head>
-//       <div>
-//         <h1 className={styles.title}>Homepage</h1>
-//         {/* <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
-//         <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p> */}
-//         <Link href="/pokemon/" className={styles.btn}>
-//           See Pokemon List
-//         </Link>
-//       </div>
-//     </>
-//   );
-// }
 import Link from "next/link";
 import { useState } from "react";
 
